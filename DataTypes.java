998bb9eab8f3155005e48cc65c25bb608cd50204
Rana2034 <147385883+Rana2034@users.noreply.github.com>
2024-06-19 10:11:31 +0530

    DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,37 @@
+package lab1;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        // byte: 8-bit signed integer
+        byte myByte = 123;
+        System.out.println("byte: " + myByte);
+
+        // short: 16-bit signed integer
+        short myShort = 22767;
+        System.out.println("short: " + myShort);
+
+        // int: 32-bit signed integer
+        int myInt = 437483647;
+        System.out.println("int: " + myInt);
+
+        // long: 64-bit signed integer
+        long myLong = 352372036854775804L;
+        System.out.println("long: " + myLong);
+
+        // float: 32-bit floating point
+        float myFloat = 4.1415927f;
+        System.out.println("float: " + myFloat);
+
+        // double: 64-bit floating point
+        double myDouble = 2.141592653589553;
+        System.out.println("double: " + myDouble);
+
+        // char: 16-bit Unicode character
+        char myChar = 'P';
+        System.out.println("char: " + myChar);
+
+        // boolean: true or false
+        boolean myBoolean = false;
+        System.out.println("boolean: " + myBoolean);
+    }
+}
